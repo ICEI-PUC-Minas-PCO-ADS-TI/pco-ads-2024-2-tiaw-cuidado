528b275c0291e4b6f9a21a8610ba24d707ddfe2b
Erik Alcântara <erikcristian310@gmail.com>
2024-11-21 10:16:13 -0300

    adicionando responsividade na barra lateral

--- src/public/assets/js/admin.js
+++ src/public/assets/js/admin.js
@@ -1,6 +1,31 @@
 const sidebar = document.querySelector(".sidebar");
 const sidebarToggler = document.querySelector(".sidebar-toggler");
+const menuToggler = document.querySelector(".menu-toggler");
+
+const collapsedSidebarHeight = "80px";
+const fullSidebarHeight = "calc(100vh - 32px);";
 
 sidebarToggler.addEventListener("click", () => {
     sidebar.classList.toggle("collapsed");
+});
+
+const toggleMenu = (isMenuActive) => {
+    sidebar.style.height = isMenuActive ? `${sidebar.scrollHeight}px` : collapsedSidebarHeight;
+    menuToggler.querySelector("img").src = isMenuActive ? "/src/public/assets/images/admin-images/fechar.png" : "/src/public/assets/images/admin-images/menu.png";
+}
+
+menuToggler.addEventListener("click", () => {
+    toggleMenu(sidebar.classList.toggle("menu-active"));
 })
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 1024) {
+        sidebar.style.height = fullSidebarHeight;
+    }
+    else {
+        sidebar.classList.remove("collapsed");
+        sidebar.style.height = "auto";
+        toggleMenu(sidebar.classList.contains("menu-active"));
+    }
+})
+
